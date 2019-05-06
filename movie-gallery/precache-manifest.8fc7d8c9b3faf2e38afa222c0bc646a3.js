3150dded1966eb9317b76b69f8382793298cfbc9
changwhaj <changwha.jeong@gmail.com>
2019-05-07 01:40:25 +0900

    Add files via upload

---
+++ movie-gallery/precache-manifest.8fc7d8c9b3faf2e38afa222c0bc646a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d239b9f58a97884c68a88149f2df52a",
+    "url": "/movie-gallery/index.html"
+  },
+  {
+    "revision": "7e1505023772b750a588",
+    "url": "/movie-gallery/static/css/main.c6d30c1b.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie-gallery/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "7e1505023772b750a588",
+    "url": "/movie-gallery/static/js/main.939054ca.chunk.js"
+  },
+  {
+    "revision": "f5b57f16c2057cb92bc6",
+    "url": "/movie-gallery/static/js/runtime~main.0b9990b9.js"
+  }
+]);
